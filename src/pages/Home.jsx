9c0d8a33d7eda42b130fdd3111c8050c272fa2f5
mtx_26 <mtx12345626@gmail.com>
2025-03-31 21:26:15 +0200

    Ajout de la structure de base de l'application React avec les pages d'accueil et d'erreur 404, ainsi que la configuration du routage.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+
+
+function Home() {
+  
+  return (
+    <div>
+      <h1>Accueil</h1>
+      <p>Bienvenue sur la page d'accueil !</p>
+    </div>
+
+
+  );
+}
+
+  export default Home;
+  
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+function NotFound() {
+    return (
+      <div>
+        <h1>Erreur 404</h1>
+        <p>Page introuvable.</p>
+      </div>
+    );
+  }
+  
+  export default NotFound;
+  
