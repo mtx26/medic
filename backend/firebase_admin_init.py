78158f9aa3549d5c069dea3c89469b091e06e34a
mtx26 <mtx12345626@gmail.com>
2025-04-20 18:00:06 +0200

    feat: Mettre à jour la gestion des clés de service Firebase pour utiliser une variable d'environnement

--- backend/firebase_admin_init.py
+++ backend/firebase_admin_init.py
@@ -1,5 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials
+import os
+# Set the environment variable for the Firebase service account key
 
-cred = credentials.Certificate("serviceAccountKey.json")
+service_account_info = os.environ.get("FIREBASE_SERVICE_ACCOUNT")
+cred = credentials.Certificate(service_account_info)
 firebase_admin.initialize_app(cred)
