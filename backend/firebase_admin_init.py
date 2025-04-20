b615967ab6c65337f6754b7139dc48850885e025
mtx26 <mtx12345626@gmail.com>
2025-04-20 18:16:32 +0200

    feat: Réorganiser l'initialisation de Firebase pour charger les variables d'environnement correctement

--- backend/firebase_admin_init.py
+++ backend/firebase_admin_init.py
@@ -1,8 +1,15 @@
+from dotenv import load_dotenv
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials
-import os
-# Set the environment variable for the Firebase service account key
 
-service_account_info = os.environ.get("FIREBASE_SERVICE_ACCOUNT")
+load_dotenv()
+
+service_account_str = os.environ.get("FIREBASE_SERVICE_ACCOUNT")
+if not service_account_str:
+    raise ValueError("FIREBASE_SERVICE_ACCOUNT is missing")
+
+service_account_info = json.loads(service_account_str)
 cred = credentials.Certificate(service_account_info)
 firebase_admin.initialize_app(cred)
