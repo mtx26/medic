6b5e004ea748c8f0beff15eb52fe08d6e753e531
mtx26 <mtx12345626@gmail.com>
2025-05-10 19:27:42 +0200

    add launch.bat

---
+++ launch.bat
@@ -0,0 +1,7 @@
+@echo off
+
+:: === Backend (Python) ===
+start "Backend" cmd /k "cd /d C:\Users\mtx_2\Documents\Code\Medic\Medic\backend && .venv\Scripts\activate && pip install -r requirements.txt && python app.py"
+
+:: === Frontend (React) ===
+start "Frontend" cmd /k "cd /d C:\Users\mtx_2\Documents\Code\Medic\Medic\frontend && powershell -Command \"Set-ExecutionPolicy Unrestricted -Scope Process\" && npm start"
