9482ec2f6262447a95d4c7ec77807dbad57f0d12
mtx26 <mtx12345626@gmail.com>
2025-05-16 22:21:26 +0200

    feat: add lodash library for utility functions and enhance WeekDayCircles component with today's highlight

--- frontend/src/components/WeekDayCircles.jsx
+++ frontend/src/components/WeekDayCircles.jsx
@@ -1,30 +1,41 @@
 export default function WeekDayCircles({ selectedDate, onSelectDate, getWeekDays }) {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0); // normalise l'heure
+  
     return (
-        <div className="d-flex justify-content-center gap-2 px-3 mb-3">
-            {getWeekDays(selectedDate).map((day, index) => {
-            const isSelected = day.toDateString() === new Date(selectedDate).toDateString();
-            return (
-                <div
-                key={index}
-                className={`text-center rounded-circle border ${isSelected ? 'bg-primary text-white' : 'bg-light'}`}
-                onClick={() => {
-                    onSelectDate(day);
-                }}
-                style={{
-                    width: 44,
-                    height: 44,
-                    lineHeight: '16px',
-                    fontSize: '0.75rem',
-                    cursor: 'pointer',
-                    paddingTop: '4px'
-                }}
-                >
-                {day.toLocaleDateString('fr-FR', { weekday: 'short' }).slice(0, 2)}
-                <br />
-                <strong>{day.getDate()}</strong>
-                </div>
-            );
-            })}
-        </div>
+      <div className="d-flex justify-content-center gap-2 px-3 mb-3">
+        {getWeekDays(selectedDate).map((day, index) => {
+          const isSelected = day.toDateString() === new Date(selectedDate).toDateString();
+          const isToday = day.toDateString() === today.toDateString();
+  
+          return (
+            <div
+              key={index}
+              className={
+                `text-center rounded-circle border 
+                ${isToday 
+                    ? 
+                    (isSelected ? 'bg-success text-white border-primary border-3' : 'bg-success text-white border-success border-3') 
+                    : 
+                    (isSelected ? 'bg-primary text-white border-primary border-3' : 'bg-light')}`
+                }
+              onClick={() => onSelectDate(day)}
+              style={{
+                width: 44,
+                height: 44,
+                lineHeight: '16px',
+                fontSize: '0.75rem',
+                cursor: 'pointer',
+                paddingTop: '4px',
+              }}
+            >
+              {day.toLocaleDateString('fr-FR', { weekday: 'short' }).slice(0, 2)}
+              <br />
+              <strong>{day.getDate()}</strong>
+            </div>
+          );
+        })}
+      </div>
     );
-}
+  }
+  
