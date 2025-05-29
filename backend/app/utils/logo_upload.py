1597486164a7f25e855b5ac1ade4c3d075078115
mtx26 <mtx12345626@gmail.com>
2025-05-29 17:45:07 +0200

    refactor: replace hardcoded warning messages with localized strings in validators and logo upload utility for improved clarity and maintainability

--- backend/app/utils/logo_upload.py
+++ backend/app/utils/logo_upload.py
@@ -1,11 +1,4 @@
-from app.utils.validators import verify_firebase_token
-from app.utils.response import success_response, error_response, warning_response
 from cloudinary.uploader import upload
-from app.utils.messages import (
-    SUCCESS_IMAGE_UPLOADED,
-    ERROR_IMAGE_UPLOAD,
-    WARNING_NO_GOOGLE_PHOTO_URL_PROVIDED
-)
 
 def upload_logo(file):
     try:
--- backend/app/utils/validators.py
+++ backend/app/utils/validators.py
@@ -1,15 +1,11 @@
 from firebase_admin import auth
 from flask import request, abort
 from app.utils.logger import log_backend as logger
-from app.utils.messages import (
-    WARNING_TOKEN_MISSING,
-    WARNING_TOKEN_INVALID
-)
 
 def verify_firebase_token():
     auth_header = request.headers.get("Authorization", "")
     if not auth_header.startswith("Bearer "):
-        logger.warning(WARNING_TOKEN_MISSING, {
+        logger.warning("token manquant ou mal formaté", {
             "origin": "TOKEN_ERROR",
             "uid": "unknown"
         })
@@ -20,7 +16,7 @@ def verify_firebase_token():
         decoded_token = auth.verify_id_token(id_token)
         return decoded_token
     except Exception:
-        logger.warning(WARNING_TOKEN_INVALID, {
+        logger.warning("token invalide ou expiré", {
             "origin": "TOKEN_ERROR",
             "uid": "unknown"
         })
