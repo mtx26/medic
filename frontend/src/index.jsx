0b1ef27c610a66863e026e96a49e80b3a4024506
mtx26 <mtx12345626@gmail.com>
2025-05-30 14:10:09 +0200

    refactor: update index.html with Open Graph meta tags for better social sharing and add local Bootstrap CSS for improved loading performance

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
+import './styles/bootstrap.min.css';
 import App from './App';
 import { UserProvider } from "./contexts/UserContext";
 
