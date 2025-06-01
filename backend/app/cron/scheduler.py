fcd8d19e4a3694715177d4e5f9b6ac9de364cfaf
mtx26 <mtx12345626@gmail.com>
2025-06-01 18:14:26 +0200

    refactor: revert scheduler to run stock decrease task daily at midnight, improving task scheduling consistency

--- backend/app/cron/scheduler.py
+++ backend/app/cron/scheduler.py
@@ -5,8 +5,8 @@ from app.cron.tasks.stock import decrease_stock
 from app.utils.logger import log_backend
 
 def run_scheduler():
-    # schedule.every(1).day.at("00:00").do(decrease_stock)
-    schedule.every(10).seconds.do(decrease_stock)
+    schedule.every(1).day.at("00:00").do(decrease_stock)
+    #schedule.every(10).seconds.do(decrease_stock)
 
     log_backend.info("⏳ [CRON] Test : cron toutes les jours initialisé")
 
