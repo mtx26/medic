5e52708c610ac68c49bbe66a291316b031bf5bf1
mtx26 <mtx12345626@gmail.com>
2025-06-08 14:53:14 +0200

    refactor: remove background message handling from Firebase messaging service worker

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -17,15 +17,3 @@ const messaging = firebase.messaging();
 // and you should use data messages for custom notifications.
 // For more info see: 
 // https://firebase.google.com/docs/cloud-messaging/concept-options
-messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/img/logo.svg'
-  };
-
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
