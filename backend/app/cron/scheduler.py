a97c4cc850fc9523b3de1bdff57cb8e0010e41d9
mtx26 <mtx12345626@gmail.com>
2025-06-29 13:39:11 +0200

    feat: update scheduler to run decrease_stock every Monday at midnight

--- backend/app/cron/scheduler.py
+++ backend/app/cron/scheduler.py
@@ -5,7 +5,11 @@ from app.cron.tasks.stock import decrease_stock
 from app.utils.logger import log_backend
 
 def run_scheduler():
-    schedule.every(1).day.at("00:00").do(decrease_stock)
+    # toute les semaines le lundi a 00:00
+    schedule.every().monday.at("00:00").do(decrease_stock)
+    # toute les jours a 00:00
+    #schedule.every(1).day.at("00:00").do(decrease_stock)
+    # toute les 10 secondes
     #schedule.every(10).seconds.do(decrease_stock)
 
     log_backend.info("⏳ [CRON] Test : cron toutes les jours initialisé")
