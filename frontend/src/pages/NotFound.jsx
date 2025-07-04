7a2939d5f913ae08b8b56f76304658e0acdba84f
mtx26 <134505471+mtx26@users.noreply.github.com>
2025-07-04 21:46:41 +0200

    Translate NotFound page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,8 +1,12 @@
+import { useTranslation } from 'react-i18next';
+
 function NotFound() {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <h1>Erreur 404</h1>
-      <p>Page introuvable.</p>
+      <h1>{t('not_found.title')}</h1>
+      <p>{t('not_found.message')}</p>
     </div>
   );
 }
