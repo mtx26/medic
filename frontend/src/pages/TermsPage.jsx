67d41b7a3e48519343398fe31d0d11a4c2e58e3b
mtx26 <mtx12345626@gmail.com>
2025-07-04 23:34:56 +0200

    remove useTranslation

--- frontend/src/pages/TermsPage.jsx
+++ frontend/src/pages/TermsPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useTranslation } from 'react-i18next';
 
 function TermsSection({ titleKey, paragraphs = [], list = [], conclusionKey }) {
   const { t } = useTranslation();
